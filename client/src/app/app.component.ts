55daf54c449f68f5e5e59436f659b386ba32cd16
homestop <homehctop@gmail.com>
2021-11-21 21:20:58 +0000

    feat(client): Added mode for mobile.
    - Make sidenav mode over for mobile screens. When mode on side mode,
    grid change own position that's may make some problems on phone.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, ChangeDetectorRef, Component, HostListener, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { RendererComponent } from '@renderer/renderer.component';
 import { RendererService } from '@renderer/services/renderer.service';
@@ -8,14 +8,30 @@ import { environment } from '../environments/environment';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   @ViewChild(RendererComponent) renderer!: RendererComponent;
   @ViewChild('sidenav') sidenav!: MatSidenav;
 
+  public sidenavMode = () => {
+    const width = window.screen.width;
+
+    if (width < 801)
+      this.sidenav.mode = 'over';
+    else
+      this.sidenav.mode = 'side';
+  };
+
   public env = environment;
 
-  constructor(public rendererService: RendererService) { }
+  constructor(public rendererService: RendererService,
+              private cdRef: ChangeDetectorRef) { }
+
+  ngAfterViewInit() {
+    this.sidenavMode();
+    this.cdRef.detectChanges();
+  }
 
   @HostListener('window:keyup.esc') closeSidenav() {
     this.sidenav.close();
